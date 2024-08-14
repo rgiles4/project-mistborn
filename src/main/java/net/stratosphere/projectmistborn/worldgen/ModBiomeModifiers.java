e1eef2eb172c8855bf346fc2d41d5ba01136473b
Ryan Giles <ryan.giles@maine.edu>
2024-08-14 13:55:13 -0400

    Ore Generation Classes
    Added classes to generate ores using Data Generation

---
+++ src/main/java/net/stratosphere/projectmistborn/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,35 @@
+package net.stratosphere.projectmistborn.worldgen;
+
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.stratosphere.projectmistborn.ProjectMistborn;
+
+public class ModBiomeModifiers {
+
+    // New ores need a resource key generated. Copy as follows for *each new ore block*
+    public static final ResourceKey<BiomeModifier> ADD_TIN_ORE = registerKey("add_tin_ore");
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+        // BiomeTags will define which biome the ore will spawn in.
+        context.register(ADD_TIN_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.TIN_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+    }
+
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(ProjectMistborn.MOD_ID, name));
+    }
+}
